4201134150d1e56d8fe0c21c7d2263270fa674af
Lucas Ikuhara <lri2911@gmail.com>
2024-04-21 00:12:52 -0300

    feat: first query processor impl

--- information-retrieval/query_processor.py
+++ information-retrieval/query_processor.py
@@ -3,6 +3,7 @@ from read_cfg import read_cfg
 import logging
 from lxml import etree
 import pandas as pd
+from unidecode import unidecode
 
 
 FORMAT = '%(asctime)s %(levelname)s: %(message)s'
@@ -20,6 +21,7 @@ def read_query(query_file: str) -> pd.DataFrame:
 
     logging.info(f"Iniciando leitura do arquivo {query_file}")
 
+    QUERY_SCHEMA = "cfcquery-2.dtd"
     class ValidationException(Exception):
         pass
 
@@ -28,7 +30,7 @@ def read_query(query_file: str) -> pd.DataFrame:
         xml = etree.parse(f)
 
     # Read schema
-    with open("data/cfc-2.dtd") as f:
+    with open(f"data/{QUERY_SCHEMA}") as f:
         dtd = etree.DTD(f)
 
     if dtd.validate(xml):
@@ -41,6 +43,45 @@ def read_query(query_file: str) -> pd.DataFrame:
     logging.info(f"Arquivo {query_file} lido com sucesso")
 
     return df
+
 # %%
-read_query("cf74.xml")
+def transform_queries(df_raw: pd.DataFrame) -> pd.DataFrame:
+    logging.info("Iniciando tranformação de consultas")
+    df = df_raw[["QueryNumber", "QueryText"]]
+
+    df.QueryText = df.QueryText.str.replace(";", "")
+    df.QueryText = df.QueryText.str.upper()
+    df.QueryText = df.QueryText.apply(lambda s: unidecode(s))
+
+    logging.info("Tranformação de consultas finalizado com sucesso")
+    return df
+
+def transform_expected_results(df_raw: pd.DataFrame) -> pd.DataFrame:
+    logging.info("Iniciando tranformação de resultados esperados")
+    df = df_raw[["QueryNumber", "DocNumber", "DocVotes"]]
+
+    logging.info("Tranformação de resultados esperados finalizado com sucesso")
+    return df
+
+# %%
+
+df = read_query("cfquery.xml")
+
+# Get file names
+queries_file = cfg["CONSULTAS"].pop()
+expected_results_file = cfg["ESPERADOS"].pop()
+
+# %%
+transform_queries(df).to_csv(
+    queries_file,
+    sep=";",
+    index=False
+)
+
+transform_expected_results(df).to_csv(
+    expected_results_file,
+    sep=";",
+    index=False
+)
+
 # %%
