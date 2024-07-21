9fdd29b456cd7b93bec691db28db4aafadd10062
Lucas Ikuhara <lri2911@gmail.com>
2024-07-21 14:04:58 -0300

    feat: add model training early stopping

--- tf/train.py
+++ tf/train.py
@@ -1,12 +1,10 @@
 # %%
 # Imports
-import tensorflow as tf
 from tensorflow import keras
 from keras.api.models import Sequential
-from scikeras.wrappers import KerasClassifier, KerasRegressor
+# from scikeras.wrappers import KerasClassifier
 from keras.api.layers import Dense, Input
-from keras import activations
-from keras.losses import MeanSquaredError
+from keras.callbacks import EarlyStopping
 import pandas as pd
 import numpy as np
 
@@ -19,23 +17,26 @@ train["embedding"] = train["embedding"].map(eval)
 x_train = np.stack(train["embedding"].map(np.array))
 
 # String labels to sparse floats
-classes = [
-    "Anxiety",
-    "Bipolar",
-    "Depression",
-    "Normal",
-    "Personality disorder",
-    "Stress",
-    "Suicidal"
-]
+def y_to_one_hot(y_df: pd.DataFrame) -> np.array:
+    classes = [
+        "Anxiety",
+        "Bipolar",
+        "Depression",
+        "Normal",
+        "Personality disorder",
+        "Stress",
+        "Suicidal"
+    ]
+
+    for cl in classes:
+        y_df[cl] = train["status"] == cl
+        y_df[cl] = y_df[cl].astype(np.float32)
+
+    y_df = y_df.drop(columns=["status"])
+    return y_df.to_numpy()
 
 y_df = train[["status"]]
-for cl in classes:
-    y_df[cl] = train["status"] == cl
-    y_df[cl] = train[cl].astype(np.float32)
-
-y_df = y_df.drop(columns=["status"])
-y_train = y_df.to_numpy()
+y_train = y_to_one_hot(y_df)
 
 # %%
 # Model creation
@@ -46,12 +47,15 @@ model = Sequential([
     Dense(7, activation="softmax")
 ])
 
-model.compile("adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model.compile("adam", loss="categorical_crossentropy", metrics=["accuracy"])
 model.summary()
 
 # %%
-# ReLu
-MAX_EPOCHS=10
-model.fit(x_train, y_train, batch_size=200, epochs=MAX_EPOCHS)
+# Train model
+early_stop = EarlyStopping(monitor='loss', patience=3)
+MAX_EPOCHS=1000
+
+model.fit(x_train, y_train, batch_size=200, epochs=MAX_EPOCHS, callbacks=[early_stop])
+model.save("/mnt/d/bmt-data/model.keras")
 
 # %%
