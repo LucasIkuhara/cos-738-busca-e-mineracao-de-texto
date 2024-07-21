07add99c53af450ecd8a5ed4b6ba5cd528a8f941
Lucas Ikuhara <lri2911@gmail.com>
2024-07-21 18:23:40 -0300

    feat: add dropout

--- tf/train.py
+++ tf/train.py
@@ -3,7 +3,7 @@
 from sklearn.model_selection import cross_val_score, train_test_split
 from keras.api.models import Sequential
 from scikeras.wrappers import KerasClassifier
-from keras.api.layers import Dense, Input
+from keras.api.layers import Dense, Input, Dropout
 from keras.callbacks import EarlyStopping
 from typing import Callable
 import keras
@@ -20,13 +20,18 @@ print("Dataset read successfully.")
 
 # %%
 # Model creation
-def make_model_sized(n: int) -> Callable[[], keras.Model]:
+def make_model_sized(
+        n: int,
+        act_fn: str
+    ) -> Callable[[], keras.Model]:
 
     def make_model() -> keras.Model:
         model = Sequential([
             Input(shape=(4096,)),
-            Dense(n, activation="relu"),
-            Dense(n, activation="relu"),
+            Dense(n, activation=act_fn),
+            Dropout(0.5),
+            Dense(n, activation=act_fn),
+            Dropout(0.5),
             Dense(7, activation="softmax")
         ])
 
@@ -45,16 +50,16 @@ BATCH_SIZE = 200
 early_stop = EarlyStopping(monitor='loss', patience=3)
 
 for size in [1024, 2048, 4096]:
-    factory = make_model_sized(size)
-    factory().summary()
+    factory = make_model_sized(size, "relu")
     estimator = KerasClassifier(
         build_fn=factory, 
         epochs=MAX_EPOCHS, 
         batch_size=BATCH_SIZE, 
-        callbacks=[early_stop]
+        callbacks=[early_stop],
+        verbose=0
     )
     sc = cross_val_score(estimator, x, y, cv=5)
 
-    print(f"[{size}-model] Accuracy Score: Mean={sc}")
+    print(f"[{size}-model] Accuracy Score: Mean={sc.mean():.3f} Std={sc.std():.3f}")
 
 # %%
